9a6158303eb7c5681cbb33c889dc725de53110da
Diego Rene Espinoza Fernandez <diespino@cbr5s6.42barcelona.com>
2026-05-08 13:19:43 +0200

    CPP01 ex02 OK

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diespino <diespino@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/08 12:26:33 by diespino          #+#    #+#             */
+/*   Updated: 2026/05/08 12:43:13 by diespino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string	brain  = "HI THIS IS BRAIN";
+	std::string	*ptr = &brain;
+	std::string	&ref = brain;
+
+	std::cout << "Dir STR: " << &brain << std::endl;
+	std::cout << "Dir PTR: " << ptr << std::endl;
+	std::cout << "Dir REF: " << &ref << "\n"<<std::endl;
+
+	std::cout << "STR: " << brain << std::endl;
+	std::cout << "PTR: " << *ptr << std::endl;
+	std::cout << "REF: " << ref << std::endl;
+	return (0);
+}
