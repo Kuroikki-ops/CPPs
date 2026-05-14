d9c65fc7756e78fc9d61f1c9a0cfffeb25535066
Diego Rene Espinoza Fernandez <diespino@cbr4s5.42barcelona.com>
2026-05-14 13:09:02 +0200

    CPP01 ex04 OK

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,70 +1,77 @@
 
-// ofstream::open / ofstream::close
 #include <iostream>
 #include <string>
 #include <fstream>      // std::ifstream, std::ofstream
-#include <cstddef>        // std::size_t
+#include <cstddef>      // std::size_t
+#include <cstdlib>	// EXIT_FAILURE, EXIT_SUCCESS
 
-int main (int argc, char **argv) {
+void	str_replace(std::ifstream& in, std::ofstream& out, std::string str1, std::string str2)
+{
+	std::cout << "str_replace" << std::endl;
+	
+	std::string	line;
+	std::string	new_line;
+	std::size_t	found;
 
-//	(void)argc;
-//	std::string		file = argv[1];
-//	std::string		new_file;
-//	std::ifstream		infile;
-//  	std::ofstream		outfile;
-//	std::size_t		found;
-//	std::string		line;
-//	std::string		new_line;
-//	std::string		str1 = argv[2];
-//	std::string		str2 = argv[3];
+	std::getline(in, line);
+	while (in.good()) 
+	{
+		found = line.find(str1);
+		while (found != std::string::npos)
+		{
+      			new_line = line.substr(0, found);
+			new_line.append(str2);
+			new_line.append(line.begin() + found + str1.length(), line.end());
+			line = new_line;
+			found = line.find(str1, found + str2.length());
+		}
+		out << line << std::endl;
+		getline(in, line);
+	}
+}
 
-	if (argc != 4 || !argv)
+int main (int argc, char **argv)
+{
+	if (argc != 4 || !argv || !argv[1][0] || !argv[2][0] || !argv[3][0])
 	{
 		std::cout << "Input error: <file> <s1> <s2>" << std::endl;
-		return (1);
+		return (EXIT_FAILURE);
 	}
 
-	std::string 	file = argv[1];
-	std::string 	str1 = argv[2];
-	std::string 	str2 = argv[3];
+	std::string	file = argv[1];
 	std::ifstream	infile;
 	
 	infile.open (file.c_str(), std::ifstream::in);
   
-	if (infile.is_open())
+	if (!infile.is_open())
 	{
-		std::size_t found = file.find_last_of(".");
-		std::string new_file = file.substr(0,found);
-		new_file.append(".replace");
+		std::cout << "Error opening file: " << file << std::endl;
+		return (EXIT_FAILURE);
+	}
 
-		std::ofstream outfile;
-		outfile.open (new_file.c_str(), std::ofstream::out | std::ofstream::trunc);
-		
-		std::string line;
-		getline(infile, line);
-		while (infile.good()) {
-			found = line.find(str1);
-			if (found != std::string::npos)
-			{
-//				std::cout << line << "\n";
-	      			std::string new_line = line.substr(0, found);
-	      			new_line.append(str2);
-//				std::cout << new_line << "\n";
-	      			new_line.append(line.begin() + found + str1.length(), line.end());
-//				std::cout << new_line << "\n";
-				outfile << new_line << std::endl;
-			}
-			else
-				outfile << line << std::endl;
-			getline(infile, line);
-		}
-		infile.close();  
-		outfile.close();
-	}
+	std::size_t	found;
+	std::string	new_file;
+	
+	found = file.find_last_of(".");
+	if (found != std::string::npos)
+		new_file = file.substr(0,found);
 	else
+		new_file = file;
+	new_file.append(".replace");
+
+	std::ofstream	outfile;
+	outfile.open (new_file.c_str(), std::ofstream::out | std::ofstream::trunc);
+	if (!outfile.is_open())
 	{
-		std::cout << "Error opening file" << std::endl;
-		return (1);
+		std::cout << "Error opening file: " << new_file << std::endl;
+		infile.close();
+		return (EXIT_FAILURE);
 	}
-	return (0);
+
+	str_replace(infile, outfile, argv[2], argv[3]);
+
+	infile.close();  
+	outfile.close();
+	
+	return (EXIT_SUCCESS);
 }
