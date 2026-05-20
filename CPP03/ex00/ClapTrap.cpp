0e175660d56234f3c34b67f2b2555f47d80972c2
Kuroikki-ops <diegoespinozafe@gmail.com>
2026-05-20 21:26:40 +0200

    falta implmentar la cannonical form en .hpp y revisar

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -6,26 +6,50 @@
 
 ClapTrap::ClapTrap(std::string name) : _name(name), _hit(10), _energy(10), _damage(0) {
 
-	std::cout << "\nClapTrap constructor called!!\n" << std::endl;
+	std::cout << "\n <! ClapTrap constructor called: [" << _name << "] has been created !>\n" << std::endl;
 }
 
 ClapTrap::~ClapTrap() {
 
-	std::cout << "\nClapTrap destructor called!!\n" << std::endl;
+	std::cout << "\n< ! ClapTrap destructor called [" << _name << "] has been destroyed !>\n" << std::endl;
 }
 
 void	ClapTrap::attack(const std::string& target)
 {
-	std::cout << "ClapTrap " << _name << "attacks " << target;
-	std::cout << ", cusing " << _damage << "points of damge!"<< std::endl;
+	if (_energy > 0 && _hit > 0)
+	{
+		_energy -= 1;
+		std::cout << "  > [ClapTrap]: " << _name << " attacks " << target << ", causing [" << _damage << "] points of damge!" << std::endl;
+		return ;
+	}
+	if (_hit <= 0)
+		std::cout << "  > [ClapTrap]: " << _name << " can't attack, is out of hit points!!" << std::endl;
+	else
+		std::cout << "  > [ClapTrap]: " << _name << " can't attack, is out of energy!!" << std::endl;
 }
 
 void	ClapTrap::takeDamage(unsigned int amount)
 {
-	std::cout << "ClapTrap " << _name << "takes " << amount << "points of damge!"<< std::endl;
+	if (_hit > 0)
+	{
+		_hit -= amount;
+		std::cout << "  > [ClapTrap]: " << _name << " takes [" << amount << "] points of damge!"<< std::endl;
+		return ;
+	}
+	std::cout << "  > [ClapTrap]: " << _name << " has no hit points!"<< std::endl;
 }
 
 void	ClapTrap::beRepaired(unsigned int amount)
 {
-	std::cout << "ClapTrap " << _name << "takes " << amount <<"heal points!"<< std::endl;
+	if (_energy > 0 && _hit > 0)
+	{
+		_energy -= 1;
+		_hit += amount;
+		std::cout << "  > [ClapTrap]: " << _name << " has repaired by him self!! Has win [" << amount <<"] hit points!"<< std::endl;
+		return ;
+	}
+	if (_hit <= 0)
+		std::cout << "  > [ClapTrap]: " << _name << " can't be repaired, is out of hit points!!" << std::endl;
+	else
+		std::cout << "  > [ClapTrap]: " << _name << " can't be repaired, is out of energy!!" << std::endl;
 }
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -1,5 +1,4 @@
 
-#include <>
 
 #include "ClapTrap.hpp"
 
@@ -7,6 +6,14 @@ int	main(void)
 {
 	ClapTrap	robot("Larry");
 
+	robot.attack("Bullymong");
+	robot.takeDamage(5);
+	robot.beRepaired(5);
+
+	robot.attack("King Mong");
+	robot.takeDamage(15);
+	robot.attack("King Mong");
+	robot.beRepaired(5);
 
 	return (0);
 }
