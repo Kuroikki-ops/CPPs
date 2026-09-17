9a83b99a4d512ee81d66f3f66cae24fe0bbd5f9c
Diego Rene Espinoza Fernandez <diespino@cbr14s6.42barcelona.com>
2026-09-17 17:49:48 +0200

    CPP08/ex00 done

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,8 +6,25 @@
 /*   By: diespino <diespino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/15 18:35:09 by diespino          #+#    #+#             */
-/*   Updated: 2026/09/15 18:35:10 by diespino         ###   ########.fr       */
+/*   Updated: 2026/09/17 17:48:00 by diespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
 
+# include <vector>
+# include <algorithm>
+# include <stdexcept>
+
+template <typename T>
+typename T::iterator	easyFind(T& container, int target) {
+
+	typename T::iterator it;
+
+	it = std::find(container.begin(), container.end(), target);
+	if (it == container.end())
+		throw (std::out_of_range("Value not found"));
+	return (it);
+}
+#endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,33 +6,66 @@
 /*   By: diespino <diespino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/15 18:35:04 by diespino          #+#    #+#             */
-/*   Updated: 2026/09/15 18:47:13 by diespino         ###   ########.fr       */
+/*   Updated: 2026/09/17 17:48:09 by diespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <exception>
 #include <vector>
+
+#include "easyfind.hpp"
 
 template <typename T>
 void	printContainer(const T& container) {
 
-	typename T::const_iterator it = container.begin();
+	typename T::const_iterator	it;
 
-	while (it != container.end())
+	std::cout << "\nContainer values: ";
+	for (it = container.begin(); it != container.end(); ++it)
 	{
-		std::cout << *it << " ";
-		it++;
+		std::cout << *it;
+		if (it != container.end() - 1)
+			std::cout << " ";
+		else
+			std::cout << "\n" << std::endl;
 	}
-	std::cout << std::endl;
 }
 
 int	main(void) {
 
-	std::vector<int>	v;
+	std::vector<int>		v;
+	std::vector<int>::iterator	it;
 
-	for (int i = 0; i < 5; i++)
+	int	v_len = 15;
+	int	test1 = 9;
+	int	test2 = 20;
+
+	for (int i = 0; i < v_len; i++)
 		v.push_back(i);
 	
 	printContainer(v);
+
+	try {
+		std::cout << "Searching value(" << test1 << ")..." << std::endl;
+		it = easyFind(v, test1);
+		std::cout << "> Value found: " << *it << "\n" << std::endl;
+	}
+	catch (const std::exception& e){
+
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+
+	try {
+		std::cout << "Searching value(" << test2 << ")..." << std::endl;
+		it = easyFind(v, test2);
+		std::cout << "> Value found: " << *it << "\n" << std::endl;
+	}
+	catch (const std::exception& e){
+
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl;
 	return (0);
 }
